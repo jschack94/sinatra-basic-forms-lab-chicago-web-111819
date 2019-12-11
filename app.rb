83b1eedfe6f653b673617e2e62959d90715c6f5b
Jonathan Schack <jschack94@gmail.com>
2019-12-11 16:27:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ get '/' do
 end
 
 get '/new' do
-  erb: create_puppy
+  erb :create_puppy
 end
 
 post '/puppy' do
   @puppy = Puppy.new(params['name'], params['breed'], params['age'])
-  erb: display_puppy
+  erb :display_puppy
   end
 
 end
